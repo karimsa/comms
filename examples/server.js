0f2bda1515e427034af2523cb5af232b3b08a0f8
Karim Alibhai <karim@alibhai.co>
2017-03-08 02:00:49 -0500

    Add basic event emitting from client to server

--- examples/server.js
+++ examples/server.js
@@ -2,8 +2,9 @@ const comms = require('../')
 const server = comms()
 
 server.on('error', console.log)
-
 server.model('message', { message: 'string' })
-server.on('message', ({ message }, who) => console.log('%s says "%s"', who, message))
+server.on('message', ({ message }, { address, port }) =>
+  console.log('%s:%s says "%s"', address, port, message)
+)
 
 server.listen(8080, () => console.log('Listening :8080'))
